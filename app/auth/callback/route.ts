dd442623ba73b16592fe907ae102213cac97ce52
Paulo Freitas <paulo.freitas@ab-inbev.com>
2024-10-30 15:53:33 -0400

    wip

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,14 +5,11 @@ export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the SSR package. It exchanges an auth code for the user's session.
   // https://supabase.com/docs/guides/auth/server-side/nextjs
+  
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
   const origin = process.env.NEXT_PUBLIC_BASE_URL;
   const redirectTo = requestUrl.searchParams.get("redirect_to")?.toString();
-
-  console.log(">>>> request", {requestURL: request.url});
-  console.log(">>>> request", {redirectTo});
-  console.log(">>>> request", {origin});
 
   if (code) {
     const supabase = await createClient();
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,16 +8,6 @@ import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
-
-export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
-};
-
 export default function RootLayout({
   children,
 }: {
@@ -37,12 +27,12 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
+                    <Link href={"/"}>🐬 Cosmic Dolphin</Link>
                   </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                  <div className="flex gap-2">
+                  <HeaderAuth />
+                  <ThemeSwitcher />
+                  </div>
                 </div>
               </nav>
               <div className="flex flex-col gap-20 max-w-5xl p-5">
@@ -50,18 +40,7 @@ export default function RootLayout({
               </div>
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
+                
               </footer>
             </div>
           </main>
