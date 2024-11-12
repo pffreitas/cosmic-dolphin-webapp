f9ec0a6f553a79a7f994666dd0f065e16df4fee5
Paulo Freitas <paulo.freitas@ab-inbev.com>
2024-11-12 07:46:53 -0600

    wip

--- app/(private)/my/dashboard/page.tsx
+++ app/(private)/my/dashboard/page.tsx
@@ -1,6 +1,17 @@
 import { fetchNotes } from "@/lib/repository/repo"
 import { Note } from "@cosmic-dolphin/api";
+import Link from "next/link";
 
+
+const NoteCard = ({ note }: { note: Note }) => {
+    return (
+        <Link href={`/notes/${note.id}`}>
+            <div className="shadow-md p-4 rounded-lg h-[200px]">
+                <h2>{note.title}</h2>
+            </div>
+        </Link>
+    )
+}
 
 export default async function Index() {
 
@@ -9,13 +20,8 @@ export default async function Index() {
 
     return (
         <div>
-            <h1>Dashboard {notes && notes.length}</h1>
-            <div>
-                {notes.map((note: Note) => (
-                    <div key={note.id}>
-                        <h2>{note.title}</h2>
-                    </div>
-                ))}
+            <div className="grid grid-cols-4 gap-4 ">
+                {notes.map((note: Note) => (<NoteCard key={note.id} note={note} />))}
             </div>
         </div>
     )
--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <main className=" h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
+            <div className="flex-1 w-full flex flex-col items-center">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
@@ -41,11 +41,11 @@ export default function RootLayout({
                   </div>
                 </div>
               </nav>
-              <div className="flex flex-col w-full max-w-5xl p-5">
+              <div className="flex flex-col w-full max-w-5xl p-5 pb-[180px] overflow-y-auto">
                 {children}
               </div>
 
-              <footer className="w-full fixed bottom-0 flex border-t text-sm gap-8 px-16 py-8">
+              <footer className="bg-white w-full fixed bottom-0 flex border-t text-sm gap-8 px-16 py-8">
                 <div className="shadow-md w-full rounded-md p-5">
                   <form className="flex gap-2">
                     <textarea name="prompt" className="w-full max-w focus:outline-none focus:border-0 resize-none" rows={1} placeholder="Type something here"></textarea>
