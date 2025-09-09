311f5b7291499206415512829ec22ee7085b780f
Paulo Freitas <paulo.freitas@ab-inbev.com>
2025-09-09 09:00:12 -0400

    refactor: enhance Tailwind CSS configuration and improve NewNoteButton styling
    - Updated tailwind.config.ts to add new animation keyframes and improve existing configurations for better UI effects.
    - Refactored NewNoteButton component to enhance loading state visuals with a new animated background and improved shadow effects.
    - Cleaned up the component structure for better readability and maintainability.

--- components/sidebar/new-note.tsx
+++ components/sidebar/new-note.tsx
@@ -61,26 +61,36 @@ export default function NewNoteButton({}: NewNoteButtonProps) {
         <div className="fixed inset-0 bg-slate-200 bg-opacity-50 backdrop-blur-sm z-50">
           <div className="fixed inset-0" onClick={handleOverlayClick}></div>
           <div>
-            <div className="absolute w-1/2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded-lg shadow-lg flex  gap-2">
-              <input
-                type="text"
-                className="w-full p-2 focus:outline-none "
-                value={url}
-                autoFocus={true}
-                disabled={createLoading}
-                onKeyDown={(e) => {
-                  if (e.key === "Enter" && !createLoading) {
-                    handleCreateBookmark();
-                  }
-                }}
-                onChange={(e) => setUrl(e.target.value)}
-                placeholder="URL"
-              />
-
-              {createError && (
-                <div className="text-red-600 p-2 text-sm">{createError}</div>
+            <div className="absolute w-1/2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              {createLoading && (
+                <div className="absolute w-full h-full blur rounded-lg bg-gradient-to-br from-pink-500 via-violet-500 to-cyan-500 animate-tilt"></div>
               )}
+              <div className="relative bg-white rounded-lg p-4 shadow-2xl shadow-teal-300">
+                <input
+                  type="text"
+                  className="w-full p-2 focus:outline-none "
+                  value={url}
+                  autoFocus={true}
+                  disabled={createLoading}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter" && !createLoading) {
+                      handleCreateBookmark();
+                    }
+                  }}
+                  onChange={(e) => setUrl(e.target.value)}
+                  placeholder="URL"
+                />
+
+                {createError && (
+                  <div className="text-red-600 p-2 text-sm">{createError}</div>
+                )}
+              </div>
             </div>
+            {/* <div className="absolute w-1/2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 blur rounded-lg bg-gradient-to-br from-pink-500 via-violet-500 to-cyan-500 animate-tilt">
+              <div className=" bg-white p-4 rounded-lg flex gap-2">
+               
+              </div>
+            </div> */}
           </div>
         </div>
       )}
